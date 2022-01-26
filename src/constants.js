8038dad8f5b1449bdf7fe56ff7cb988151e308cb
Andrew Morozov <Loutrec4763662@>
2022-01-26 18:26:08 +0300

    дз выполнено

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const firstName = 'Повара'
+const secondName = 'блюдо'
+const dz_name = 'Ресторан'
+const student = 'Андрей Морозов'
+const first_form_first = 'полное имя повара'
+const second_form_first = 'название блюда'
+const second_form_second = ''
+const second_form_third = ''
+const domain = 'http://localhost:8000/api'
+
+export {firstName, secondName, domain, dz_name, student, first_form_first, second_form_second, second_form_third, second_form_first}
